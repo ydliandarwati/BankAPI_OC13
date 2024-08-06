cf5654f656008408e9b8a42e0d512dc8b923da77
Yunita Dwi Liandarwati <ydliandarwati@gmail.com>
2024-08-07 00:27:55 +0200

    added redux store

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+      <Provider store={store}>
         <HashRouter>
           <App />
         </HashRouter>
+      </Provider>	
 );
---
+++ src/redux/store.jsx
@@ -0,0 +1,15 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+// import { authReducer } from './reducers/auth.reducer.jsx';
+// import { userReducer } from './reducers/user.reducer.jsx';
+
+const rootReducer = combineReducers({
+//    auth: authReducer,
+//    user: userReducer
+})
+
+const store = configureStore({
+    reducer: rootReducer,
+    devTools: true 
+})
+
+export default store
